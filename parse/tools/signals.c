915b9b5d34ae0082c6a9b184cecd8fd988e55cf5
kaafkhar <afkharkaoutar18@gmail.com>
2024-10-22 16:31:53 +0100

    ss

--- parse/tools/signals.c
+++ parse/tools/signals.c
@@ -6,7 +6,7 @@
 /*   By: kaafkhar <kaafkhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 22:59:18 by kaafkhar          #+#    #+#             */
-/*   Updated: 2024/10/14 02:39:11 by kaafkhar         ###   ########.fr       */
+/*   Updated: 2024/10/21 16:31:51 by kaafkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,8 @@ void sigint_handler(int signum)
     rl_redisplay();
 }
 
-void sigquit_handler(int signum) 
-{
-    (void)signum;
-    write(STDOUT_FILENO, "Quit (core dumped)\n", 20);
-}
-
 void setup_signal_handlers()
 {
     signal(SIGINT, sigint_handler);
-    signal(SIGQUIT, sigquit_handler);
+    sigignore(SIGQUIT);
 }
