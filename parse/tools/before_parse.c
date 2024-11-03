ff779d8027436bddd9d7b4b0331eefc83bbbfc48
kaafkhar <afkharkaoutar18@gmail.com>
2024-11-03 20:56:13 +0100

    SHELLVL

--- parse/tools/before_parse.c
+++ parse/tools/before_parse.c
@@ -6,7 +6,7 @@
 /*   By: kaafkhar <kaafkhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 05:34:46 by ychagri           #+#    #+#             */
-/*   Updated: 2024/11/03 19:36:03 by kaafkhar         ###   ########.fr       */
+/*   Updated: 2024/11/03 20:47:37 by kaafkhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,47 @@ char	*envgetter(const char *key, t_list *env)
 	free(lookup);
 	return (value);
 }
+
+void	add_env_node(t_list **env, char *new_content)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	new_node->content = new_content;
+	new_node->next = *env;
+	*env = new_node;
+}
+
+void init_shlvl(t_list **env)
+{
+    t_list *shlvl_node;
+    int shlvl_value;
+    char *new_shlvl;
+    char *itoa_value;
+
+    shlvl_node = find_env_node2(*env, "SHLVL=");
+    if (shlvl_node)
+    {
+        shlvl_value = ft_atoi(shlvl_node->content + 6);
+        shlvl_value++;
+        itoa_value = ft_itoa(shlvl_value);
+        new_shlvl = ft_strjoin("SHLVL=", itoa_value);
+        free(itoa_value);
+        if (!new_shlvl)
+            return;
+        free(shlvl_node->content);
+        shlvl_node->content = new_shlvl;
+    }
+    else
+    {
+        new_shlvl = ft_strdup("SHLVL=1");
+        if (new_shlvl)
+            add_env_node(env, new_shlvl);
+    }
+}
+
 
 void	set_envi(t_args *cmd_line)
 {
@@ -79,4 +120,5 @@ void	environment(char **envp, t_args *cmd_line)
 		ft_lstadd_back(&cmd_line->env, node);
 		i++;
 	}
+	init_shlvl(&cmd_line->env);
 }
